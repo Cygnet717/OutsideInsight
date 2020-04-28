36114e7d498fb584176a86667bd960ec40647be8
Kathy Bradt <kathybradt717@gmail.com>
2020-04-27 22:43:59 -0500

    functioning burger button for mobile view

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     $('.burgerMenu').on('click', event => {
-        console.log('clicked burger')
+        $('.linkBox').addClass('burgerDropdown').css({'display': 'flex'})
+        $('.windowCover').css({'display': 'block'})
     })
+
+    $('.windowCover').on('click', event => {
+        $('.windowCover').css({'display': 'none'})
+        $('.linkBox').removeClass('burgerDropdown').css({'display': 'none'})
+    })
+
 })
